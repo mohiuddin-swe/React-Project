6d1f49890027136aa3d6272e29e2d65566d69ac9
Mohi Uddin <mohiuddin.swe@gmail.com>
2023-08-17 04:10:24 +0600

    Project Structure

---
+++ vite-project/src/Pages/ByCategorypage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ByCategorypage = () => {
+    return (
+        <div>
+           <h1>This is ByCategoryPage</h1> 
+        </div>
+    );
+};
+
+export default ByCategorypage;
